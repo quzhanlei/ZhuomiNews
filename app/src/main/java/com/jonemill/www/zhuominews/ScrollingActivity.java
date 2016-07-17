94425735788135bc94939d55738e93e71c21856f
jonemill <jonemill@qq.com>
2016-07-18 02:03:05 +0800

    测试提交

--- app/src/main/java/com/jonemill/www/zhuominews/ScrollingActivity.java
+++ app/src/main/java/com/jonemill/www/zhuominews/ScrollingActivity.java
@@ -15,6 +15,7 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        //Toolbar 是个控件!
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
